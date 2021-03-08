1318a3360c3932df2cd7af8c88380c111e6169db
killLeo <79762071+killLeo@users.noreply.github.com>
2021-03-08 17:39:36 +0100

    Update defaultmodules.js

--- modules/default/defaultmodules.js
+++ modules/default/defaultmodules.js
@@ -7,7 +7,7 @@
 
 // Modules listed below can be loaded without the 'default/' prefix. Omitting the default folder name.
 
-var defaultModules = ["MMM-cryptocurrency","MMM-CoinMarketCap", "clock", "currentweather","newsfeed","MMM-Paris-RATP-PG"];
+var defaultModules = ["MMM-cryptocurrency","MMM-CoinMarketCap", "clock", "currentweather","newsfeed","MMM-Paris-RATP-PG","MMM-Ratp-master"];
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
